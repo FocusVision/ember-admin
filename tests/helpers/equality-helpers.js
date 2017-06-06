d6bb6beed7e3aeaf32f2d76c53ebf885a9bc8aa8
ksure <kyle.scherling@gmail.com>
2017-06-05 23:11:40 -0700

    Use spread operators more better

--- tests/helpers/equality-helpers.js
+++ tests/helpers/equality-helpers.js
@@ -1,31 +1,24 @@
 export { rowValuesEqual, inputPropertiesEqual }
 
-function rowValuesEqual(...args) {
-  const [expect, row] = [...args]
-  const values = [...args].slice(2, [...args].length)
+function rowValuesEqual(expect, row, ...args) {
+  const values = args
   const columns = row.find('th, td')
 
   expect(columns.length).to.equal(values.length)
-  // `expected ${values.length} columns: (${values.join(', ')})`
 
   for (let i = 0; i < columns.length; i++) {
     const columnText = columns.eq(i).text().trim()
     expect(columnText).to.equal(values[i])
-    // `expected column(${(i + 1)}) with value: ${values[i]}`
   }
 }
 
-function inputPropertiesEqual(...args) {
-  const [expect, inputs] = [...args]
-  const values = [...args].slice(2, [...args].length)
+function inputPropertiesEqual(expect, inputs, ...args) {
+  const values = args
 
   expect(inputs.length).to.equal(values.length)
-  // `expected ${values.length} inputs: (${values.join(', ')})`
 
   for (let i = 0; i < inputs.length; i++) {
     const labelText = inputs.eq(i).parent().text().trim()
-
     expect(labelText).to.equal(values[i])
-    // `expected input(${(i + 1)}) for property: ${values[i]}`
   }
 }
