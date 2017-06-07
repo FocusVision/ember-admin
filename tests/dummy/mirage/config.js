1cc80c6fb3e45abc5f95af897a5b7f6285e74a3d
ksure <kyle.scherling@gmail.com>
2017-06-06 17:59:28 -0700

    Update patch method to be model agnostic

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,18 +1,26 @@
+const jsonapiType = request => JSON.parse(request.requestBody).data.type
+const patch = function(schema, request) {
+  const { params: { id }} = request
+  return schema[jsonapiType(request)]
+    .find(id)
+    .update(this.normalizedRequestAttrs())
+}
+
 export default function() {
   this.namespace = '/admin'
 
   this.get('/cats')
   this.post('/cats')
   this.get('/cats/:id')
-  this.patch('/cats/:id', function({ cats }, { params: { id }}) {
-    return cats.find(id).update(this.normalizedRequestAttrs())
-  })
+  this.patch('/cats/:id', patch)
   this.delete('/cats/:id')
 
   this.get('/dogs')
+  this.post('/dogs')
   this.get('/dogs/:id')
 
   this.get('/birds')
+  this.post('/birds')
   this.get('/birds/:id')
 
   this.get('/toys')
@@ -20,6 +28,7 @@ export default function() {
   this.get('/toys/:id')
 
   this.get('/owners')
+  this.post('/owners')
   this.get('/owners/:id')
 
   this.get('/courses')
