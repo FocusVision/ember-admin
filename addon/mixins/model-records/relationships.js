5f7cbd3fcc391006b492d1f3af9e17e66cb2123b
ksure <kyle.scherling@gmail.com>
2017-06-11 11:52:01 -0700

    Reduce relationship building complexity, dont preload models

--- addon/mixins/model-records/relationships.js
+++ addon/mixins/model-records/relationships.js
@@ -7,46 +7,24 @@ const {
   isArray,
   getOwner,
   Mixin,
-  A: emberArray
+  A
 } = Ember
 
-function relationshipMacro(type) {
-  return computed('recordType', 'id', function() {
-    return get(this, 'model')
-      ._debugInfo()
-      .propertyInfo
-      .groups
-      .filter(group => group.name === type)
-      .reduce((relationships, group) => {
-        group.properties.forEach(property => {
-          get(this, `model.${property}`).then(records => {
-            // This might have to be written in such a way
-            // as to provide an observer for 'model.'+property
-            // and push onto the array when that property is available
-            if (!isArray(records)) {
-              records = emberArray([records])
-            }
-
-            const store = getOwner(this).lookup('store:admin')
-            const constructor = get(this, 'model.constructor')
-            const inverse = constructor.inverseFor(property, store)
-            const meta = constructor.metaForProperty(property)
-
-            pushObject(relationships, {
-              name: property,
-              records,
-              type: meta.type,
-              inverse: inverse && inverse.name
-            })
-          })
-        })
-
-        return relationships
-      }, emberArray())
+const relationshipsFor = (model, kind) => {
+  const relationshipsForKind = []
+  model.get('constructor.relationshipsByName').forEach((relationship, name) => {
+    if (relationship.kind === kind) {
+      relationshipsForKind.push(relationship)
+    }
   })
+  return A(relationshipsForKind)
 }
 
 export default Mixin.create({
-  hasMany: relationshipMacro('Has Many'),
-  belongsTo: relationshipMacro('Belongs To')
+  hasMany: computed('model', function() {
+    return relationshipsFor(this.get('model'), 'hasMany')
+  }),
+  belongsTo: computed('model', function() {
+    return relationshipsFor(this.get('model'), 'belongsTo')
+  }),
 })
