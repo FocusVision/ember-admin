5ed9f2642348928535795ce5cdcebfbc858ba737
Rick Sullivan <rsullivan@focusvision.com>
2017-08-29 11:48:11 -0700

    Use admin store

--- addon/mixins/admin-model-mixin.js
+++ addon/mixins/admin-model-mixin.js
@@ -2,17 +2,22 @@ import Ember from 'ember'
 import HasManyQuery from 'ember-data-has-many-query'
 
 const {
-  Mixin
+  Mixin,
+  inject: { service }
 } = Ember
 const { ModelMixin } = HasManyQuery
 
 export default Mixin.create(ModelMixin, {
+  admin: service(),
+
   removeRelated(...args) {
-    const adapter = this.store.adapterFor(this.constructor.modelName)
+    const adapter =
+      this.get('admin.store').adapterFor(this.constructor.modelName)
     return adapter.removeRelated(this, ...args)
   },
   addRelated(...args) {
-    const adapter = this.store.adapterFor(this.constructor.modelName)
+    const adapter =
+      this.get('admin.store').adapterFor(this.constructor.modelName)
     return adapter.addRelated(this, ...args)
   }
 })
